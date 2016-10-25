73d023f4ba508974c907147060a8ec3784a68ffb
JoonHo Park <pjhjohn@gmail.com>
2016-10-25 12:03:54 +0000

    Fix typo : episodess, add views for browser access

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -4,7 +4,7 @@ class EpisodesController < ApplicationController
   
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @episodess }
+      format.json { render json: @episodes }
     end
   end
   
