a0c03e4bcff6d4657c5bffa287a79fdc43a24ab1
JoonHo Park <pjhjohn@gmail.com>
2016-11-02 11:24:31 +0000

    Set autoload_paths in application.rb config

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Auto load lib modules (reference : http://stackoverflow.com/questions/19098663/auto-loading-lib-files-in-rails-4)
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
