208bc2489c8bb23c024d721fa0840fa72ca818a6
JoonHo Park <pjhjohn@gmail.com>
2016-11-07 17:39:47 +0000

    Separated data update functions from actions
    - Updates all following data after the point of data stream
    - Also clips out values in diff_states
    - Also separated math functions for normalizing & clipping

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,50 +1,33 @@
 class EpisodesController < ApplicationController
-  def json_deep_serialize (episode)
-    return {
-      :id             => episode.id,
-      :name           => episode.name,
-      :timestep       => episode.timestep,
-      :control_points => JSON.parse(episode.control_points),
-      :states         => JSON.parse(episode.states),
-      :diff_states    => JSON.parse(episode.diff_states),
-      :commands       => JSON.parse(episode.commands),
-      :created_at     => episode.created_at,
-      :updated_at     => episode.updated_at,
-    }
-  end
-
-  def clip_timestep (timestep)
-    return 40 if timestep < 40
-    return 200 if timestep > 200
-    return timestep
-  end
-
   def index
     @episodes = Episode.all
-  
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @episodes.map{|episode| json_deep_serialize(episode)} }
     end
   end
-  
+
   def new
     @episode = Episode.new
-  
+
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: json_deep_serialize(@episode) }
     end
   end
-  
+
   def create
     @episode = Episode.new
     @episode.name = params[:name]
-    @episode.timestep = clip_timestep(params[:timestep].to_i) unless params[:timestep].nil?
+    @episode.timestep = clip(params[:timestep].to_i, 80, 120) unless params[:timestep].nil?
     @episode.control_points = JSON.parse(params[:control_points]).to_json # Checks JSON validity during conversion
 
     respond_to do |format|
       if @episode.save
+        update_episode_states(@episode)
+        update_episode_diff_states(@episode)
+        update_episode_commands(@episode)
         flash[:notice] = 'Episode was successfully created.'
         format.html { redirect_to(@episode) }
         format.json { render json: json_deep_serialize(@episode), status: :created, location: @episode }
@@ -57,7 +40,7 @@ class EpisodesController < ApplicationController
   
   def show
     @episode = Episode.find(params[:id])
-  
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: json_deep_serialize(@episode) }
@@ -73,11 +56,14 @@ class EpisodesController < ApplicationController
 
     episode_params = Hash.new
     episode_params[:name] = params[:name] unless params[:name].nil?
-    episode_params[:timestep] = clip_timestep(params[:timestep].to_i) unless params[:timestep].nil?
+    episode_params[:timestep] = clip(params[:timestep].to_i, 80, 120) unless params[:timestep].nil?
     episode_params[:control_points] = JSON.parse(params[:control_points]).to_json # Checks JSON validity during conversion
-  
+
     respond_to do |format|
       if @episode.update(episode_params)
+        update_episode_states(@episode)
+        update_episode_diff_states(@episode)
+        update_episode_commands(@episode)
         flash[:notice] = 'Episode was successfully updated.'
         format.html { redirect_to(@episode) }
         format.json { head :ok }
@@ -87,7 +73,7 @@ class EpisodesController < ApplicationController
       end
     end
   end
-  
+
   def destroy
     @episode = Episode.find(params[:id])
     @episode.destroy
@@ -98,9 +84,31 @@ class EpisodesController < ApplicationController
     end
   end
 
+  ## Actions for Episode Data Update ##
   def update_states
     @episode = Episode.find(params[:id])
-    control_points = JSON.parse(@episode.control_points)
+    update_episode_states(@episode)
+    update_episode_diff_states(@episode)
+    update_episode_commands(@episode)
+    redirect_to(@episode)
+  end
+
+  def update_diff_states
+    @episode = Episode.find(params[:id])
+    update_episode_diff_states(@episode)
+    update_episode_commands(@episode)
+    redirect_to(@episode)
+  end
+
+  def update_commands
+    @episode = Episode.find(params[:id])
+    update_episode_commands(@episode)
+    redirect_to(@episode)
+  end
+
+  ## Functions for Episode Data Update ##
+  def update_episode_states(episode)
+    control_points = JSON.parse(episode.control_points)
     bez = Bezier::Curve.new(*control_points.map{ |point| [point["x"], point["y"], point["z"], point["r"]]})
     max_time = control_points.last["t"]
 
@@ -115,21 +123,18 @@ class EpisodesController < ApplicationController
         :z => point.z,
         :r => point.r,
       })
-      time += @episode.timestep
+      time += episode.timestep
     end
 
-    @episode.states = states.to_json
-    @episode.save
-
-    redirect_to(@episode)
+    episode.states = states.to_json
+    episode.save
   end
 
-  def update_diff_states
-    @episode = Episode.find(params[:id])
-    states = JSON.parse(@episode.states)
+  def update_episode_diff_states(episode)
+    states = JSON.parse(episode.states)
 
     diff_states = (1...states.length).map do |i|
-      prev, curr, timestep_s = states[i-1], states[i], @episode.timestep / 1000.0
+      prev, curr, timestep_s = states[i-1], states[i], episode.timestep / 1000.0
 
       # Position Coordinate : World -> Body
       dx = (curr["x"] - prev["x"]) / timestep_s
@@ -141,35 +146,21 @@ class EpisodesController < ApplicationController
         [0, 0, 1]                                           # Rotation axis
       )
 
-      hash = {
+      {
         :t  => prev["t"],
-        :dx => v_body[0],
-        :dy => v_body[1],
-        :dz => v_body[2],
-        :w  => (curr["r"] - prev["r"]) / timestep_s
+        :dx => clip(v_body[0],                             15.0), # Will convert into Roll
+        :dy => clip(v_body[1],                             15.0), # Will convert into Pitch
+        :dz => clip(v_body[2],                              4.0), # Will convert into Throttle
+        :w  => clip((curr["r"] - prev["r"]) / timestep_s, 100.0), # Will convert into Yaw
       }
     end
 
-    @episode.diff_states = diff_states.to_json
-    @episode.save
-
-    redirect_to(@episode)
+    episode.diff_states = diff_states.to_json
+    episode.save
   end
 
-  ## Limitations of Drone (both positive & negative)
-  # Yaw : 100degrees/s
-  # Pitch Roll : 15m/s
-  # Throttle : 4m/s
-  def normalized_clip(value, limit)
-    clipped_val = value
-    clipped_val = limit if value > limit
-    clipped_val = -limit if value < -limit
-    return clipped_val / limit
-  end
-
-  def update_commands
-    @episode = Episode.find(params[:id])
-    diff_states = JSON.parse(@episode.diff_states)
+  def update_episode_commands(episode)
+    diff_states = JSON.parse(episode.diff_states)
 
     commands = diff_states.map do |diff_state|
       {
@@ -181,9 +172,42 @@ class EpisodesController < ApplicationController
       }
     end
 
-    @episode.commands = commands.to_json
-    @episode.save
+    episode.commands = commands.to_json
+    episode.save
+  end
 
-    redirect_to(@episode)
+  ## Calculations ##
+  ## Limitations of Drone (both positive & negative)
+  # Yaw : 100degrees/s
+  # Pitch Roll : 15m/s
+  # Throttle : 4m/s
+  def clip(value, limit, offset = 0)
+    clipped_val = value
+    clipped_val = offset + limit if value > offset + limit
+    clipped_val = offset - limit if value < offset - limit
+    return clipped_val
+  end
+
+  def clipped?(value, limit, offset = 0)
+    return value == clip(value, limit, offset)
+  end
+
+  def normalized_clip(value, limit)
+    return clip(value, limit) / limit
+  end
+
+  ## Serialization ##
+  def json_deep_serialize (episode)
+    return {
+      :id             => episode.id,
+      :name           => episode.name,
+      :timestep       => episode.timestep,
+      :control_points => JSON.parse(episode.control_points),
+      :states         => JSON.parse(episode.states),
+      :diff_states    => JSON.parse(episode.diff_states),
+      :commands       => JSON.parse(episode.commands),
+      :created_at     => episode.created_at,
+      :updated_at     => episode.updated_at,
+    }
   end
 end
